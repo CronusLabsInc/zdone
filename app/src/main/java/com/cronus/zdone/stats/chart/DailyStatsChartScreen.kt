3291cf493325b398bea877cdab77fdde9005dc75
William Clausen <william.h.f.clausen@gmail.com>
2020-03-21 18:09:14 -0700

    added and styled chart, fake data currently used

--- app/src/main/java/com/cronus/zdone/stats/chart/DailyStatsChartScreen.kt
+++ app/src/main/java/com/cronus/zdone/stats/chart/DailyStatsChartScreen.kt
@@ -3,6 +3,7 @@ package com.cronus.zdone.stats.chart
 import android.content.Context
 import com.cronus.zdone.CoroutineScreen
 import javax.inject.Inject
+import kotlin.random.Random
 
 class DailyStatsChartScreen @Inject constructor(): CoroutineScreen<DailyStatsChartView>() {
 
@@ -11,11 +12,15 @@ class DailyStatsChartScreen @Inject constructor(): CoroutineScreen<DailyStatsCha
     override fun getTitle(context: Context?) = "stats"
 
     override fun onShow(context: Context) {
-
+        val fakeChartData = List(13) {
+            HoursWorkedData(Random.nextDouble(.5, 7.5))
+        }
+        view?.setWeeklyHoursWorked(fakeChartData, List(14) { 5f } )
     }
 
 }
 
+data class HoursWorkedData(val hoursWorked: Double)
 
 
 
